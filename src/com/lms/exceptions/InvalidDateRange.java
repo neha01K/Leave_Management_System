cf94ea7a0c83a71633941197530e42bab5da83bb
neha01K <khandelwal.neha6441@gmail.com>
2025-08-18 11:31:40 +0530

    RunTime Exception Handling added

---
+++ src/com/lms/exceptions/InvalidDateRange.java
@@ -0,0 +1,26 @@
+package com.lms.exceptions;
+
+
+import java.time.LocalDate;
+
+public class InvalidDateRange extends LMSException{
+
+    private LocalDate startDate;
+    private LocalDate endDate;
+    public InvalidDateRange(LocalDate startDate, LocalDate endDate){
+        super(String.format("Invalid Date Range: Start Date %s is before End Date %s", startDate, endDate));
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public InvalidDateRange(String message){
+        super(message);
+    }
+
+    public LocalDate getStartDate(){
+        return startDate;
+    }
+    public LocalDate getEndDate(){
+        return endDate;
+    }
+}
